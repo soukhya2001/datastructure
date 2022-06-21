350ed9d0edf1b1604c5c725293ecb181351d4ac7
soukhya2001 <78601416+soukhya2001@users.noreply.github.com>
2022-06-21 20:30:40 +0530

    Add files via upload

--- LinkedList/SingleLinkedList/Node.java
+++ LinkedList/SingleLinkedList/Node.java
@@ -54,6 +54,78 @@ class Main{
 	    	 i++;
 	     }while(i<=n);
 	     temp.traverse(head);
+	     System.out.println("Enter position Where you want to insert the element:");
+	     int pos=sc.nextInt();
+	     if(pos>n || pos<1)
+	     {
+	    	 System.out.println("Invalid Position:");
+	     }
+	     if(pos==1)
+	     {
+	     InsertAtBegining obj=new InsertAtBegining();
+		 head=obj.insert(head);
+		 
+		 temp.traverse(head);
+		 n++;
+	     }
+	     else if(pos==n)
+	     {
+	     InsertAtEnd obj2=new InsertAtEnd();
+		temp= obj2.insertend(temp);
+		temp.traverse(head);
+		n++;
+	     }
+	     
+	     else
+	     {
+	    	 InsertAtPosition obj3=new InsertAtPosition();
+	    	 head=obj3.insertatpos(head, pos);
+	    	 temp.traverse(head);
+	    	 n++;
+	     }
+	     System.out.println("Enter position form where you want to delete");
+	     int delpos=sc.nextInt();
+	     if(delpos>n || delpos<1)
+	     {
+	    	 System.out.println("Invalid Position:");
+	     }
+	     else if(delpos==1)
+	     {
+	     DeleteAtBegining obj4=new DeleteAtBegining();
+	      head=obj4.deleteatbegin(head);
+	      temp.traverse(head);
+	      n--;
+	      }
+	     if(delpos==n)
+	     {
+	    	 DeleteAtEnd obj5=new DeleteAtEnd();
+	    	 temp=obj5.deleteatend(head, temp);
+	    	  temp.traverse(head);
+	    	  n--;
+	     }
+	     else
+	     {
+	    	 DeleteAtPositon obj6=new DeleteAtPositon();
+	    	head= obj6.deleteatpos(head, delpos);
+	    	temp.traverse(head);
+	    	n--;
+	    	 
+	     }
+	     System.out.println("Enter element you want to search");
+	     int find=sc.nextInt();
+	     Search obj7=new Search();
+	      int n1=obj7.search(head, find);
+	      if(n1==0)
+	      {
+	    	  System.out.println("Element not found");
+	      }
+	      else
+	      {
+	    	  System.out.println("Element found at Location="+n);
+	      }
+	      
+	     
+	     
 	}
 
 
